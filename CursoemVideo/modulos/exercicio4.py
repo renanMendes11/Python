93f4dcbe74d9d5df0f726b73f7befcca715a5606
renanmendes11 <renanmende049@gmail.com>
2017-10-29 19:15:23 -0300

    manipulando strings

---
+++ CursoemVideo/modulos/exercicio4.py
@@ -0,0 +1,2 @@
+city = str(input('Em que cidade voce nasceu ? ')).strip()
+print(city[:5].upper() == 'SANTO')
--- CursoemVideo/strings/basico.py
+++ CursoemVideo/strings/basico.py
@@ -5,6 +5,6 @@ frase = "Curso em Video Python"
 ##print(frase[5:3])
 ##printfrase[9:]
 ##print(frase[9::3])
-
-print(len(frase))
-print(frase.count('o', 0, 14))
+num = len(frase)
+print(num)
+print(frase.count('o', 0, num))
